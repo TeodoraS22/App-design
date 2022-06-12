75d4b3e42d4c7fe278163ca2eba9dc22ac41f19a
flaxi-cj <pop.flaviu.cj@gmail.com>
2022-06-12 17:28:15 +0300

    Finished minigame functionality and styling. Added gitignore

--- src/components/pages/MiniGame/miniGameQuestions.js
+++ src/components/pages/MiniGame/miniGameQuestions.js
@@ -1,222 +1,233 @@
-export const miniGameQuestions = [
+export const miniGameQuestions = {
 
-    {   questionNumber: 1,
-        category: 'Kitchen',
-        question: "Cum se monteaza lustrele de tip pendul?",
-        answers: [
-            {
-                text: "La maxim 15 cm fata de marginea blatului.",
-                correct: false,
-            },
-            {
-                text: "La minim 15 cm fata de marginea blatului.",
-                correct: true,
-            },
-            {
-                text: "La 20 cm fata de centrul blatului.",
-                correct: false,
-            },
-        ]
-    },
+    'Kitchen': [
+        {
+            questionNumber: 1,
+            question: "Cum se monteaza lustrele de tip pendul?",
+            answers: [
+                {
+                    text: "La maxim 15 cm fata de marginea blatului.",
+                    correct: false,
+                },
+                {
+                    text: "La minim 15 cm fata de marginea blatului.",
+                    correct: true,
+                },
+                {
+                    text: "La 20 cm fata de centrul blatului.",
+                    correct: false,
+                },
+            ]
+        },
 
-    {
-        questionNumber: 2,
-        category: 'Kitchen',
-        question: "Cum este indicata montarea prizelor pentru siguranta blatului de lucru?",
-        answers: [
-            {
-                text: "La maxim 30 cm de plita si chiuveta si maxim 15 cm de blat.",
-                correct: false,
-            },
-            {
-                text: "La minim 30 cm de plita si chiuveta si minim 15 cm de blat.",
-                correct: true,
-            },
-            {
-                text: "Montarea prizelor poate fi realizata in orice loc, la orice distanta.",
-                correct: false,
-            },
-        ]
-    },
+        {
+            questionNumber: 2,
+            question: "Cum este indicata montarea prizelor pentru siguranta blatului de lucru?",
+            answers: [
+                {
+                    text: "La maxim 30 cm de plita si chiuveta si maxim 15 cm de blat.",
+                    correct: false,
+                },
+                {
+                    text: "La minim 30 cm de plita si chiuveta si minim 15 cm de blat.",
+                    correct: true,
+                },
+                {
+                    text: "Montarea prizelor poate fi realizata in orice loc, la orice distanta.",
+                    correct: false,
+                },
+            ]
+        },
 
-    {
-        questionNumber: 3,
-        category: 'Kitchen',
-        question: "Cum trebuie sa fie raportul dintre latimea hotei si plita?",
-        answers: [
-            {
-                text: "Dimensiunea hotei sa fie mai mica decat suprafata plitei de gatit.",
-                correct: false,
-            },
-            {
-                text: "Dimensiunea hotei sa nu fie mai mica decat suprafata plitei de gatit.",
-                correct: true,
-            },
-            {
-                text: "Dimensiunea hotei sa fie egala cu suprafata plitei de gatit.",
-                correct: false,
-            },
-        ]
-    },
+        {
+            questionNumber: 3,
+            question: "Cum trebuie sa fie raportul dintre latimea hotei si plita?",
+            answers: [
+                {
+                    text: "Dimensiunea hotei sa fie mai mica decat suprafata plitei de gatit.",
+                    correct: false,
+                },
+                {
+                    text: "Dimensiunea hotei sa nu fie mai mica decat suprafata plitei de gatit.",
+                    correct: true,
+                },
+                {
+                    text: "Dimensiunea hotei sa fie egala cu suprafata plitei de gatit.",
+                    correct: false,
+                },
+            ]
+        },
 
-    {
-        questionNumber: 4,
-        category: 'Kitchen',
-        question: "Care este iluminarea ideala in bucatarie pentru a evita crearea umbrelor pe blatul de lucru?",
-        answers: [
-            {
-                text: "Folosirea unei singure surse de lumina.",
-                correct: false,
-            },
-            {
-                text: "Folosirea mai multor surse de lumina.",
-                correct: true,
-            },
-            {
-                text: "Food",
-                correct: false,
-            },
-        ]
-    },
+        {
+            questionNumber: 4,
+            question: "Care este iluminarea ideala in bucatarie pentru a evita crearea umbrelor pe blatul de lucru?",
+            answers: [
+                {
+                    text: "Folosirea unei singure surse de lumina.",
+                    correct: false,
+                },
+                {
+                    text: "Folosirea mai multor surse de lumina.",
+                    correct: true,
+                },
+                {
+                    text: "Food",
+                    correct: false,
+                },
+            ]
+        },
 
-    {
-        questionNumber: 5,
-        category: 'Kitchen',
-        question: "Cum trebuie pozitionate electrocasnicele?",
-        answers: [
-            {
-                text: "Pozitionate mai jos de nivelul braului.",
-                correct: false,
-            },
-            {
-                text: "Pozitionate la nivelul braului, pentru a fi accesate cu usurinta.",
-                correct: true,
-            },
-            {
-                text: "Pozitionare in orice mod.",
-                correct: false,
-            },
-        ]
-    },
+        {
+            questionNumber: 5,
+            question: "Cum trebuie pozitionate electrocasnicele?",
+            answers: [
+                {
+                    text: "Pozitionate mai jos de nivelul braului.",
+                    correct: false,
+                },
+                {
+                    text: "Pozitionate la nivelul braului, pentru a fi accesate cu usurinta.",
+                    correct: true,
+                },
+                {
+                    text: "Pozitionare in orice mod.",
+                    correct: false,
+                },
+            ]
+        },
 
-    {
-        questionNumber: 6,
-        category: 'Kitchen',
-        question: "Care este distanta minima recomandata dintre electrocasnice?",
-        answers: [
-            {
-                text: "50 cm",
-                correct: false,
-            },
-            {
-                text: "30 cm",
-                correct: true,
-            },
-            {
-                text: "20 cm",
-                correct: false,
-            },
-        ]
-    },
+        {
+            questionNumber: 6,
+            question: "Care este distanta minima recomandata dintre electrocasnice?",
+            answers: [
+                {
+                    text: "50 cm",
+                    correct: false,
+                },
+                {
+                    text: "30 cm",
+                    correct: true,
+                },
+                {
+                    text: "20 cm",
+                    correct: false,
+                },
+            ]
+        },
 
-    {
-        questionNumber: 7,
-        category: 'Kitchen',
-        question: "Unde se pozitioneaza lustra de tip pendul?",
-        answers: [
-            {
-                text: "In centrul bucatariei, la o distanta de 70-90 cm de tavan.",
-                correct: false,
-            },
-            {
-                text: "Deasupra mesei, la o distanta de 70-90 cm de masa.",
-                correct: true,
-            },
-            {
-                text: "Lustra de tip pendul poate fi pozitionata in orice loc, la orice inaltime.",
-                correct: false,
-            },
-        ]
-    },
+        {
+            questionNumber: 7,
+            question: "Unde se pozitioneaza lustra de tip pendul?",
+            answers: [
+                {
+                    text: "In centrul bucatariei, la o distanta de 70-90 cm de tavan.",
+                    correct: false,
+                },
+                {
+                    text: "Deasupra mesei, la o distanta de 70-90 cm de masa.",
+                    correct: true,
+                },
+                {
+                    text: "Lustra de tip pendul poate fi pozitionata in orice loc, la orice inaltime.",
+                    correct: false,
+                },
+            ]
+        },
 
-    {
-        questionNumber: 8,
-        category: 'Kitchen',
-        question: "Care este inaltimea potrivita a mesei?",
-        answers: [
-            {
-                text: "60 cm",
-                correct: false,
-            },
-            {
-                text: "70 cm",
-                correct: true,
-            },
-            {
-                text: "75 cm",
-                correct: false,
-            },
-        ]
-    },
+        {
+            questionNumber: 8,
+            question: "Care este inaltimea potrivita a mesei?",
+            answers: [
+                {
+                    text: "60 cm",
+                    correct: false,
+                },
+                {
+                    text: "70 cm",
+                    correct: true,
+                },
+                {
+                    text: "75 cm",
+                    correct: false,
+                },
+            ]
+        },
 
-    {
-        questionNumber: 9,
-        category: 'Kitchen',
-        question: "Care este inaltimea de sezut a scaunelor potrivita?",
-        answers: [
-            {
-                text: "40 cm",
-                correct: false,
-            },
-            {
-                text: "45 cm",
-                correct: true,
-            },
-            {
-                text: "50 cm",
-                correct: false,
-            },
-        ]
-    },
+        {
+            questionNumber: 9,
+            question: "Care este inaltimea de sezut a scaunelor potrivita?",
+            answers: [
+                {
+                    text: "40 cm",
+                    correct: false,
+                },
+                {
+                    text: "45 cm",
+                    correct: true,
+                },
+                {
+                    text: "50 cm",
+                    correct: false,
+                },
+            ]
+        },
 
-    {
-        questionNumber: 10,
-        category: 'Kitchen',
-        question: "Distanta optima dintre blatul de bucatarie si corpurile d emobilier suspendate este de?",
-        answers: [
-            {
-                text: "50 cm",
-                correct: false,
-            },
-            {
-                text: "60 cm",
-                correct: true,
-            },
-            {
-                text: "70 cm",
-                correct: false,
-            },
-        ]
-    },
-    {
-        questionNumber: 1,
-        category: 'Livingroom',
-        question: "eeeeeeeeeeeeeeeeeeellllllll?",
-        answers: [
-            {
-                text: "50 cm",
-                correct: false,
-            },
-            {
-                text: "60 cm",
-                correct: true,
-            },
-            {
-                text: "70 cm",
-                correct: false,
-            },
-        ]
-    },
+        {
+            questionNumber: 10,
+            question: "Distanta optima dintre blatul de bucatarie si corpurile d emobilier suspendate este de?",
+            answers: [
+                {
+                    text: "50 cm",
+                    correct: false,
+                },
+                {
+                    text: "60 cm",
+                    correct: true,
+                },
+                {
+                    text: "70 cm",
+                    correct: false,
+                },
+            ]
+        },],
+    "Living room": [
+        {
+            questionNumber: 1,
+            question: "eeeeeeeeeeeeeeeeeeellllllll?",
+            answers: [
+                {
+                    text: "50 cm",
+                    correct: false,
+                },
+                {
+                    text: "60 cm",
+                    correct: true,
+                },
+                {
+                    text: "70 cm",
+                    correct: false,
+                },
+            ]
+        },
+        {
+            questionNumber: 2,
+            question: "sadasda?",
+            answers: [
+                {
+                    text: "50 cm",
+                    correct: false,
+                },
+                {
+                    text: "60 cm",
+                    correct: true,
+                },
+                {
+                    text: "70 cm",
+                    correct: false,
+                },
+            ]
+        },
+    ]
 
-];
+};
 
